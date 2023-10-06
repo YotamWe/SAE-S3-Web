06c94871baafb6ba718f22c6a36c1b8724e2b5a4
YotamWe <yotam.weber@etu.u-paris.fr>
2023-10-06 11:48:58 +0200

    scroll navbar

--- Site Web/js/file.js
+++ Site Web/js/file.js
@@ -4,3 +4,12 @@ function divSearch() {
 }
 
 document.getElementById("btnSearch").addEventListener("click", divSearch);
+
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > 0 && window.scrollY >= navbar.offsetHeight) {
+      navbar.classList.add("scrolled");
+  } else {
+      navbar.classList.remove("scrolled");  
+  }
+});
