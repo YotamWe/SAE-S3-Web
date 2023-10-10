2f0cbd40d6b68abd8cc16bc12ce3a98980ea4699
YotamWe <yotam.weber@etu.u-paris.fr>
2023-10-10 14:15:58 +0200

    début theme sombre

--- Site Web/js/file.js
+++ Site Web/js/file.js
@@ -23,3 +23,19 @@ function toggleNav(){
   hamburgerButton.classList.toggle("active");
   navigation.classList.toggle("active");
 }
+
+const themeBtn = document.querySelector(".theme-btn");
+const icon = themeBtn.querySelector("i");
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+      if (icon.classList.contains("bi-brightness-high-fill")) {
+        icon.classList.remove("bi-brightness-high-fill");
+        icon.classList.add("bi-moon-fill");
+      } else {
+        icon.classList.remove("bi-moon-fill");
+        icon.classList.add("bi-brightness-high-fill");
+      }
+      /*fa-solid fa-moon*/
+      
+});
