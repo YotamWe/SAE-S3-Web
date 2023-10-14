7077b1eb171c4cd01157054b5ca2dbd0c7d3b0b7
YotamWe <yotam.weber@etu.u-paris.fr>
2023-10-14 18:47:35 +0100

    ajout effet elu + mise en page elu

--- Site Web/js/file.js
+++ Site Web/js/file.js
@@ -1,36 +1,37 @@
-const navbar = document.querySelector("nav");
-window.addEventListener("scroll", function() {
-  const navbar = document.querySelector("nav");
+/*Agrandissement de la navbar au scroll*/
+
+var navbar = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
   if (window.scrollY > 0 && window.scrollY >= navbar.offsetHeight) {
-      navbar.classList.add("scrolled");
+    navbar.classList.add("scrolled");
   } else {
-      navbar.classList.remove("scrolled");  
+    navbar.classList.remove("scrolled");
   }
 });
 
+/******************************************************************************************************** */
+
+
+
+/*Ajout d'accueil dans la navbar quand l'écran rétrécit + toggle menu-hamburger active*/
+
 const hamburgerButton = document.querySelector(".btnBurger");
-const navigation = document.querySelector("nav");
-
 let accueilCreated = false;
-hamburgerButton.addEventListener("click", toggleNav);
 
-function toggleNav(){
-  if (document.body.style.overflowY == "hidden"){
-    document.body.style.overflowY = "visible"; 
-  }
-  else{
+function toggleNav() {
+  if (document.body.style.overflowY == "hidden") {
+    document.body.style.overflowY = "visible";
+  } else {
     document.body.style.overflowY = "hidden";
   }
   hamburgerButton.classList.toggle("active");
-  navigation.classList.toggle("active");
-  const navbar = document.querySelector("nav");
-
+  navbar.classList.toggle("active");
 
   if (!accueilCreated) {
-
     let accueil = document.createElement("li");
     accueil.id = "accueil";
-    let a  = document.createElement("a");
+    let a = document.createElement("a");
     a.textContent = "Accueil";
     a.href = "accueil.html";
     let ul = navbar.querySelector("ul");
@@ -39,12 +40,17 @@ function toggleNav(){
     ul.prepend(accueil);
     // Mise à jour de l'état de l'input
     accueilCreated = true;
-
   }
-
 }
 
-window.addEventListener("resize", function() {
+hamburgerButton.addEventListener("click", toggleNav);
+
+
+/******************************************************************************************************** */
+
+
+/*Suppression d'accueil dans la navbar quand l'écran s'agrandit*/
+window.addEventListener("resize", function () {
   // Vérification de la taille de la page
   let width = window.innerWidth;
   // Si la taille de la page atteint le niveau souhaité
@@ -55,21 +61,27 @@ window.addEventListener("resize", function() {
   }
 });
 
+
+/******************************************************************************************************** */
+
+
+/*Thème sombre / thème clair*/
 const themeBtn = document.querySelector(".theme-btn");
 const icon = themeBtn.querySelector("i");
 themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
 
-      if (icon.classList.contains("bi-brightness-high-fill")) {
-        icon.classList.remove("bi-brightness-high-fill");
-        icon.classList.add("bi-moon-fill");
-      } else {
-        icon.classList.remove("bi-moon-fill");
-        icon.classList.add("bi-brightness-high-fill");
-      }
-      
+  if (icon.classList.contains("bi-brightness-high-fill")) {
+    icon.classList.remove("bi-brightness-high-fill");
+    icon.classList.add("bi-moon-fill");
+  } else {
+    icon.classList.remove("bi-moon-fill");
+    icon.classList.add("bi-brightness-high-fill");
+  }
 });
 
+/******************************************************************************************************** */
+/*Activation toggle SearchBar sur la navbar*/
 const searchBarContainerEl = document.querySelector(".search-bar-container");
 
 const magnifierEl = document.querySelector("#magnifier");
@@ -77,3 +89,55 @@ const magnifierEl = document.querySelector("#magnifier");
 magnifierEl.addEventListener("click", () => {
   searchBarContainerEl.classList.toggle("active");
 });
+
+/******************************************************************************************************** */
+
+/*Jeu d'affichage dans Elu*/
+const greeting = ["Les Élus", "vous souhaitent", "la Bienvenue !"];
+let currentGreetingIndex = 0;
+let currentCharacterIndex = 0;
+let isDeleting = false;
+let isPaused = false;
+let pauseEnd = 0;
+
+function typeWriterEffect() {
+  const greetingElement = document.getElementById("titre");
+
+  if (isPaused && Date.now() > pauseEnd) {
+    isPaused = false;
+    if (isDeleting) {
+      currentGreetingIndex = (currentGreetingIndex + 1) % greeting.length;
+      isDeleting = false;
+    } else {
+      isDeleting = true;
+    }
+  }
+
+  if (
+    !isPaused &&
+    !isDeleting &&
+    currentCharacterIndex === greeting[currentGreetingIndex].length
+  ) {
+    isPaused = true;
+    pauseEnd = Date.now() + 800;
+    return setTimeout(typeWriterEffect, 50);
+  }
+
+  if (!isPaused && isDeleting && currentCharacterIndex === 0) {
+    isPaused = true;
+    pauseEnd = Date.now() + 200;
+    return setTimeout(typeWriterEffect, 50);
+  }
+
+  const timeout = isDeleting ? 100 : 200;
+  greetingElement.innerText = greeting[currentGreetingIndex].substring(
+    0,
+    currentCharacterIndex
+  );
+  currentCharacterIndex = isDeleting
+    ? currentCharacterIndex - 1
+    : currentCharacterIndex + 1;
+  setTimeout(typeWriterEffect, timeout);
+}
+
+typeWriterEffect();
