83a9d3d1adb91166693224f1b388d40f9db23387
theMatuvu <matthieuju1@gmail.com>
2023-10-26 10:07:46 +0200

    contact final

--- js/contact.js
+++ js/contact.js
@@ -1,38 +1,50 @@
+const greeting2 = ["Contactez nous !"];
+let currentGreetingIndex2 = 0;
+let currentCharacterIndex2 = 0;
+let isDeleting2 = false;
+let isPaused2 = false;
+let pauseEnd2 = 0;
 
+function typeWriterEffectContact() {
+  const greetingElement2 = document.getElementById("TitreContact");
 
-function typeWriter(text, index = 0) {
-  var speed = 50; // typing speed in milliseconds
-  var element = document.getElementById("TitreContact");
-  element.textContent = " "
-  if (index < text.length) {
-    element.textContent += text.charAt(index);
-    index++;
-    setTimeout(function() {
-      typeWriter(text, index);
-    }, speed); 
-  } else {
-    setTimeout(function() {
-      deleteText(text, index);
-    }, speed * 10); // pause before deleting
+  if (isPaused2 && Date.now() > pauseEnd2) {
+    isPaused2 = false;
+    if (isDeleting2) {
+      currentGreetingIndex2 = (currentGreetingIndex2 + 1) % greeting2.length;
+      isDeleting2 = false;
+    } else {
+      isDeleting2 = true;
+    }
   }
-}
 
-function deleteText(text, index) {
-  var speed = 25; // deletion speed in milliseconds
-  var element = document.getElementById("TitreContact");
-
-  if (index >= 0) {
-    element.textContent = text.substring(0, index);
-    index--;
-    setTimeout(function() {
-      deleteText(text, index);
-    }, speed);
-  } else {
-    setTimeout(function() {
-      typeWriter(text, 0); // restart typing
-    }, speed * 10); // pause before typing
+  if (
+    !isPaused2 &&
+    !isDeleting2 &&
+    currentCharacterIndex2 === greeting2[currentGreetingIndex2].length
+  ) {
+    isPaused2 = true;
+    pauseEnd2 = Date.now() + 800;
+    return setTimeout(typeWriterEffectContact, 50);
   }
-}
 
-// Call the function with your desired text
-typeWriter("Contact");
+  if (!isPaused2 && isDeleting2 && currentCharacterIndex2 === 0) {
+    isPaused2 = true;
+    pauseEnd2 = Date.now() + 200;
+    return setTimeout(typeWriterEffectContact, 50);
+  }
+
+  const timeout = isDeleting2 ? 100 : 200;
+  greetingElement2.innerText = greeting2[currentGreetingIndex2].substring(
+    0,
+    currentCharacterIndex2
+  );
+  currentCharacterIndex2 = isDeleting2
+    ? currentCharacterIndex2 - 1
+    : currentCharacterIndex2 + 1;
+  setTimeout(typeWriterEffectContact, timeout);
+}
+window.addEventListener("load", function() {
+  // Votre code JavaScript ici
+  typeWriterEffectContact();
+});
